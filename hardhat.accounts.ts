7dca77acc7048e28dd491eb613e56d5a32209e7e
AlejoLovallo <alejoe.lovallo@gmail.com>
2024-02-29 12:20:21 -0300

    sepolia deploy

--- hardhat.accounts.ts
+++ hardhat.accounts.ts
@@ -7,7 +7,8 @@ const namedAccounts = {
   },
   treasury: {
     default: 2,
-    80001: '0x153073310327caA6abB76F735d115E5c8bA1617B'
+    80001: '0x153073310327caA6abB76F735d115E5c8bA1617B',
+    1115511: '0x32413920CD9861f88c9ca9a29e0fAe0Bb85C750a'
   },
   token: {
     default: 3,
